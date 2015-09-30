515638e445761d5b19f92f65656e2bd42679bb2b
Chris Bodley <chris.matthew.bodley@gmail.com>
2015-09-30 02:49:14 +0000

    this took way too long to figure out.

--- public/scripts/PlayerObj.js
+++ public/scripts/PlayerObj.js
@@ -1,21 +1,33 @@
 function player(texture,startx, starty, name)
 {
+    //for the object when cloning
     this.texture = texture;
+    
+    //sets up the object for rendering in pixi
     this.obj = new PIXI.Sprite(this.texture);
     this.obj.anchor.x = 0.5;
     this.obj.anchor.y = 0.5;
-    this.vx = 0;
-    this.vy = 0;
     this.obj.position.x = startx;
     this.obj.position.y = starty;
+    
+    //we want it to go in a direction these two properties will help with that
+    this.vx = 0;
+    this.vy = 0;
     this.velocity = 2;
+    
+    //some properties to help with logic
     this.crashed = false;
     this.playerName = name;
-}
+    
+    //for our hit box our sprites are 25pix by 25pix and the origin should be in the center
+    //25 / 2 = 12.5 but we will round up
+    this.offset = 6;
+    
+    //this will be instantiated but will change
+    this.futurexy = new PIXI.Point(startx,starty);
+    this.minus = true;
 
-player.prototype.setContainer = function(container){
-    container.addChild(this.obj);
-};
+}
 
 player.prototype.start = function(container, direction){
     container.addChild(this.obj);
@@ -23,42 +35,51 @@ player.prototype.start = function(container, direction){
         case 'up':
             // code
             this.up();
+            this.futurexy = new PIXI.Point(this.obj.x, this.obj.y - this.offset);
             break;
         case 'down':
             // code
             this.down();
+            this.futurexy = new PIXI.Point(this.obj.x, this.obj.y + this.offset);
             break;
         case 'left':
             this.left();
+            this.futurexy = new PIXI.Point(this.obj.x - this.offset , this.obj.y);
             break;
         case 'right':
             // code
             this.right();
+            this.futurexy = new PIXI.Point(this.obj.x  + this.offset , this.obj.y);
             break;
         default:
             this.right();
+            this.futurexy = new PIXI.Point(this.obj.x  + this.offset , this.obj.y);
             // code
     }
 };
 
 player.prototype.left = function(){
     this.vx = this.velocity * -1;
+    this.minus = true;
     this.vy = 0;
 };
 
 player.prototype.right = function(){
     this.vx = this.velocity;
+    this.minus = false;
     this.vy = 0;     
 };
 
 player.prototype.up = function(){
     this.vx = 0;
-    this.vy = this.velocity * -1;    
+    this.vy = this.velocity * -1;   
+    this.minus = true;
 };
 
 player.prototype.down = function(){
     this.vx = 0;
-    this.vy = this.velocity;     
+    this.vy = this.velocity;
+    this.minus = false;
 };
 
 player.prototype.update = function(container){
@@ -75,6 +96,12 @@ player.prototype.update = function(container){
                     
         this.obj.x += this.vx;
         this.obj.y += this.vy;
+        
+        if(this.vx !== 0){
+            this.futurexy.x += this.vx;
+        }else if(this.vy !== 0){
+            this.futurexy.y  += this.vy;
+        }
     }
 };
 
@@ -84,26 +111,80 @@ player.prototype.allstop = function(){
     this.velocity = 0;
 };
 
-player.prototype.crash = function(container, limitx, limity){
+//I going to call this offGrid
+player.prototype.offGrid = function(container, limitx, limity){
     var check = false;
-    if(this.obj.x > limitx || this.obj.x < 0){
+    if(this.futurexy.x > limitx + this.offset || this.futurexy.x < 0){
         check = true;
     }
-    if(this.obj.y < 0 || this.obj.y >limity){
+    if(this.futurexy.y < 0 || this.futurexy.y > limity + this.offset){
         check = true;
     }
     
     return check;
 };
 
-player.prototype.hasCrashed = function(container, resx, resy){
-    var check = this.crash(container, resx, resy)
+
+player.prototype.intersects = function(container){
+    for(var i = 0; i<container.children.length; i++){
+        var item = container.children[i];
+        
+        var xdist = 0;
+        if(this.vx !== 0){
+            if(this.minus){
+                xdist = item.position.x - (this.obj.position.x - this.offset);
+            }else{
+                xdist = item.position.x - (this.obj.position.x + this.offset);
+            }
+            
+        }else{
+            xdist = item.position.x - this.obj.position.x
+        }
+        
+        if(xdist > -item.width/2 && xdist < item.width/2)
+		{
+            var ydist = 0;
+            if(this.vy !== 0){
+                if(this.minus){
+                    ydist = item.position.y - (this.obj.position.y - this.offset);
+                }else{
+                    ydist = item.position.y - (this.obj.position.y + this.offset);
+                }
                 
+            }else{
+                ydist = item.position.y - this.obj.position.y;
+            }
+
+           
+            if(ydist > -item.height/2 && ydist < item.height/2)
+			{
+                return true;
+			}
+		}
+    }
+    return false;
+};
+//if it crashed then let the player know
+player.prototype.hasCrashed = function(container, resx, resy){
+    var check = this.offGrid(container, resx, resy);
+                    
     if(check){
+        if(!this.crashed){
+            this.allstop();
+            this.crashed = true;
+            alert(this.playerName + " Has Went Off Grid");
+        }
+    }
+    
+    check = this.intersects(container);
+    
+        if(check){
         if(!this.crashed){
             this.allstop();
             this.crashed = true;
             alert(this.playerName + " Has Crashed");
         }
     }
+    
+    return check;
 };
